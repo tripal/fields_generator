9d737701972229bcdbe8b440b2db14e86d498d87
Abdullah Almsaeed <almasaeed2010@gmail.com>
2017-08-21 10:02:11 -0400

    Create options parser

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -4,6 +4,7 @@
  * Include all needed files.
  */
 
+include __DIR__ . '/OptionsParser.php';
 include __DIR__ . '/CLIPrompt.php';
 include __DIR__ . '/Colors.php';
 include __DIR__ . '/Generator.php';
